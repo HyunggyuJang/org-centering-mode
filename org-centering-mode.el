e9788bcf90d1942764a9e9d6d189ad2e1bb25a2c
Hyunggyu Jang <murasakipurplez5@gmail.com>
2021-07-05 13:47:13 +0900

    Implemented centering latex fragment with numbering label.

--- org-centering-mode.el
+++ org-centering-mode.el
@@ -45,18 +45,20 @@ activated."
   ;; Activate global effects.
   (when (not org-centering--buffers)
     (org-centering--set-global-effects))
-  (push (current-buffer) org-centering--buffers))
+  (push (current-buffer) org-centering--buffers)
+  (org-centering-enable-inlinefrags))
 
 (defun org-centering--set-global-effects ()
   "Activate global effects."
-  (advice-add #'org-display-inline-images :around #'+org-inlineimage-ensure-centering-a)
-  (advice-add #'org--make-preview-overlay :after #'+org-latex-ensure-centering-a))
+  (advice-add #'org-display-inline-images :around #'org-centering-ensure-inlineimage-centering-a)
+  (advice-add #'org--make-preview-overlay :after #'org-centering-ensure-latex-centering-a))
 
 (defun org-centering--disable ()
   "Reset the current buffer to its normal appearance.
 Also run the functions in `org-centering-global-effects' to undo
 their effects if `org-centering-mode' is deactivated in the last
 buffer in which it was active."
+  (org-centering-disable-inlinefrags)
   ;; Restore global effects if necessary.
   (setq org-centering--buffers (delq (current-buffer) org-centering--buffers))
   (when (not org-centering--buffers)
@@ -64,14 +66,13 @@ buffer in which it was active."
 
 (defun org-centering--unset-global-effects ()
   "Deactivate global effects."
-  (advice-remove #'org-display-inline-images #'+org-inlineimage-ensure-centering-a)
-  (advice-remove #'org--make-preview-overlay #'+org-latex-ensure-centering-a))
+  (advice-remove #'org-display-inline-images #'org-centering-ensure-inlineimage-centering-a)
+  (advice-remove #'org--make-preview-overlay #'org-centering-ensure-latex-centering-a))
 
 ;;;###autoload
 (define-minor-mode org-centering-mode
   "Minor mode for centering org mode inline overlays."
   :init-value nil :lighter nil :global nil
-  (+org-toggle-inlinefrags-center-h org-centering-mode)
   (if org-centering-mode
       (org-centering--enable)
     (org-centering--disable)))
@@ -102,7 +103,7 @@ This has no side effect. If DELETE? is true and works like `org-plist-delete'."
         plist
       `(,property ,value . ,plist))))
 
-(defun +org-inlineimage-ensure-centering-a (orig-fn &rest args)
+(defun org-centering-ensure-inlineimage-centering-a (orig-fn &rest args)
   (if org-centering-mode
       (let
           ((overlay-put
@@ -125,35 +126,73 @@ This has no side effect. If DELETE? is true and works like `org-plist-delete'."
           (apply orig-fn args)))
     (apply orig-fn args)))
 
-(defun +org-latex-ensure-centering-a (beg end image &optional imagetype)
+(defcustom org-centering-numbering-environments-regexp
+  (regexp-opt (mapcar (lambda (env) (format "\\begin{%s}" env)) '("align" "equation" "gather")))
+  "LaTeX fragments with numbering label.")
+
+(defsubst org-centering--inline-math? (beg)
+  "Test whether BEG is a starting delimiter of inline math fragment."
+  (or (eq (char-after beg) ?$)
+      (eq (char-after (1+ beg)) ?\() ;assume first is \\
+      ))
+
+(defun org-centering-ensure-latex-centering-a (beg end image &optional imagetype)
   (if org-centering-mode
-      (unless (or (eq (char-after beg) ?$)
-                  (eq (char-after (1+ beg)) ?\() ;assume first is \\
-                  )
+      (unless (org-centering--inline-math? beg)
         (let* ((ov (cl-find-if (lambda (o) (overlay-get o 'org-overlay-type)) (overlays-at beg)))
                (img (overlay-get ov 'display))
+               (img-width (car (image-size img 'pixel)))
+               width)
+          (if (string-match org-centering-numbering-environments-regexp
+                            (buffer-substring-no-properties
+                             beg
+                             (save-excursion (end-of-line) (point))))
+              (setq width (* 2 (- img-width 530)))
+            (setq width img-width))
+          (overlay-put ov 'before-string
+                       (make-string (round (- (window-text-width nil 'pixel)
+                                              width)
+                                           (* 2 7))
+                                    ? ))))))
+
+(defun org-centering-enable-inlinefrags ()
+  "Enable centering for existing inline fragments in current visible buffer."
+  (dolist (ov (ignore-errors (overlays-in (point-min) (point-max))))
+    (if (overlay-get ov 'org-image-overlay)
+        (let* ((img (overlay-get ov 'display))
                (width (car (image-size img 'pixel)))
                (offset (max (floor (/ (- (window-text-width nil 'pixel) width) 2)) 0)))
-          (setq img (cons (car img)  (org-centering-plist-update (cdr img) :margin nil (cons offset 0))))
-          (overlay-put ov 'display img)))))
+          (overlay-put ov 'display
+                       (cons (car img)  (org-centering-plist-update (cdr img) :margin nil (cons offset 0)))))
+      (if (eq (overlay-get ov 'org-overlay-type) 'org-latex-overlay)
+          (let ((beg (overlay-start ov)))
+            (if (not (org-centering--inline-math? beg))
+                (let* ((img (overlay-get ov 'display))
+                       (img-width (car (image-size img 'pixel)))
+                       width)
+                  (if (string-match org-centering-numbering-environments-regexp
+                                    (buffer-substring-no-properties
+                                     beg
+                                     (save-excursion (end-of-line) (point))))
+                      (setq width (* 2 (- img-width 530)))
+                    (setq width img-width))
+                  (overlay-put ov 'before-string
+                               (make-string (round (- (window-text-width nil 'pixel)
+                                                      width)
+                                                   (* 2 7))
+                                            ? )))))))))
 
-(defun +org-toggle-inlinefrags-center-h (enable?)
-  (dolist (ov (cl-remove-if-not
-               (lambda (o) (or
-                       (overlay-get o 'org-image-overlay)
-                       (and (eq (overlay-get o 'org-overlay-type) 'org-latex-overlay)
-                            (not (let ((beg (overlay-start o)))
-                                   (or (eq (char-after beg) ?$)
-                                       (eq (char-after (1+ beg)) ?\() ;assume first is \\
-                                       ))))))
-               (ignore-errors (overlays-in (point-min) (point-max)))))
-    (let ((img (overlay-get ov 'display)))
-      (if enable?
-          (let* ((width (car (image-size img 'pixel)))
-                 (offset (max (floor (/ (- (window-text-width nil 'pixel) width) 2)) 0)))
-            (setq img (cons (car img)  (org-centering-plist-update (cdr img) :margin nil (cons offset 0)))))
-        (setq img (cons (car img) (org-centering-plist-update (cdr img) :margin 'delete))))
-      (overlay-put ov 'display img))))
+(defun org-centering-disable-inlinefrags ()
+  "Disable centering for existing inline fragments in current visible buffer."
+  (dolist (ov (ignore-errors (overlays-in (point-min) (point-max))))
+    (if (overlay-get ov 'org-image-overlay)
+        (let ((img (overlay-get ov 'display)))
+          (overlay-put ov 'display
+                       (cons (car img) (org-centering-plist-update (cdr img) :margin 'delete))))
+      (if (eq (overlay-get ov 'org-overlay-type) 'org-latex-overlay)
+          (let ((beg (overlay-start ov)))
+            (if (not (org-centering--inline-math? beg))
+                (overlay-put ov 'before-string "")))))))
 
 (provide 'org-centering-mode)
 ;;; org-centering-mode.el ends here
